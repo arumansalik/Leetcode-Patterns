8015647f4caa251ae320b2dbd8df0caafca10241
unknown <arumansalik45@gmail.com>
2025-07-29 23:52:27 +0530

    Binary SubArray With Sum

---
+++ SlidingWindow/FixedWindow/BinarySubarraysWithSum.java
@@ -0,0 +1,34 @@
+package SlidingWindow.FixedWindow;
+
+
+
+public class BinarySubarraysWithSum {
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+
+    private static int atMost(int[] nums, int k) {
+        if (k < 0) return 0;
+
+        int left = 0, sum = 0, count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+
+            while (sum > k) {
+                sum -= nums[left++];
+            }
+
+            count += right - left + 1;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 1, 0, 1};
+        int goal = 2;
+        int ans = numSubarraysWithSum(arr, goal);
+        System.out.println(ans);
+    }
+}
