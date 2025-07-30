07ddb1c3e8126bdc70726d2abc8e7318010f993f
unknown <arumansalik45@gmail.com>
2025-07-31 01:06:52 +0530

    Max Consecutive Ones

--- SlidingWindow/FixedWindow/MaxConsecutiveOnes.java
+++ SlidingWindow/FixedWindow/MaxConsecutiveOnes.java
@@ -1,20 +1,30 @@
 package SlidingWindow.FixedWindow;
 
 public class MaxConsecutiveOnes {
-    public int longestOnes(int[] nums, int k) {
-        int left = 0, maxLen = 0;
+    public static int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int maxLen = 0;
 
-        for (int right = 0; right < nums.length; right++) {
-            if (nums[right] == 0) k--;
-
-            while (k < 0) {
-                if (nums[left] == 0) k++;
-                left++;
+        for(int right = 0; right < nums.length; right++) {
+            if(nums[right] == 0) {
+                k--;
             }
 
+            while(k < 0) {
+                if(nums[left] == 0) {
+                    k++;
+                }
+                left++;
+            }
             maxLen = Math.max(maxLen, right - left + 1);
         }
-
         return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+        int k = 2;
+        int ans = longestOnes(nums, k);
+        System.out.println(ans);
     }
 }
