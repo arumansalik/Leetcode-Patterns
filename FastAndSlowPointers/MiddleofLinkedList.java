69cbd0da6d3dfc587c34024d705689bfe8344f90
unknown <arumansalik45@gmail.com>
2025-08-12 16:13:51 +0530

    Middle of the Linked List

---
+++ FastAndSlowPointers/MiddleofLinkedList.java
@@ -0,0 +1,32 @@
+package FastAndSlowPointers;
+
+public class MiddleofLinkedList {
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        // Create example linked list: 1 -> 3 -> 5 -> 7 -> 9
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(5);
+        ListNode node4 = new ListNode(7);
+        ListNode node5 = new ListNode(9);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        ListNode middle = middleNode(node1);
+
+        System.out.println("The middle node value is: " + middle.val);
+    }
+}
