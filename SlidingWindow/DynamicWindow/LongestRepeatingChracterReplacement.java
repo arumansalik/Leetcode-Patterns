65add10970ae6e55666e16e9ec8a25146f30db1f
unknown <arumansalik45@gmail.com>
2025-08-23 10:46:52 +0530

    Longest Repeating Character Replacement

--- SlidingWindow/DynamicWindow/LongestRepeatingChracterReplacement.java
+++ SlidingWindow/DynamicWindow/LongestRepeatingChracterReplacement.java
@@ -3,15 +3,15 @@ package SlidingWindow.DynamicWindow;
 public class LongestRepeatingChracterReplacement {
     public static int characterReplacement(String s, int k) {
         int[] count = new int[26];
-        int maxCount = 0;
+        int maxFreq = 0;
         int left = 0, maxLen = 0;
 
         for(int right = 0; right < s.length(); right++) {
             count[s.charAt(right) - 'A']++;
 
-            maxCount = Math.max(maxCount, count[s.charAt(right) - 'A']);
+            maxFreq = Math.max(maxFreq, count[s.charAt(right) - 'A']);
 
-            while((right - left + 1) - maxCount > k) {
+            while((right - left + 1) - maxFreq > k) {
                 count[s.charAt(left) - 'A']--;
                 left++;
             }
