5ef4dfc5d61ebc407365784d5ee9857b5882c9ea
unknown <arumansalik45@gmail.com>
2025-09-02 15:50:21 +0530

    Min Window SubSTring

--- SlidingWindow/DynamicWindow/MinimumWindowSubstring.java
+++ SlidingWindow/DynamicWindow/MinimumWindowSubstring.java
@@ -4,53 +4,29 @@ import java.util.HashMap;
 
 public class MinimumWindowSubstring {
     public static String minWindow(String s, String t) {
-        if (s == null || t == null || s.length() < t.length()) return "";
+        int m = s.length();
+        int n = t.length();
 
-        // Store character frequencies of t
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            map.put(c, map.getOrDefault(c, 0) + 1);
+        if(m < n) return "";
+
+        int[] hashArr = new int[128];
+        for (int i = 0; i < n; i++) {
+            hashArr[t.charAt(i)]++;
         }
 
-        int left = 0, right = 0, count = t.length();
+        int l = 0, r = 0;
+        int count = 0;
         int minLen = Integer.MAX_VALUE;
-        int start = 0;
+        int sIndex = -1;
 
-        while (right < s.length()) {
-            char c = s.charAt(right);
-            if (map.containsKey(c)) {
-                if (map.get(c) > 0) {
-                    count--; // valid character matched
-                }
-                map.put(c, map.get(c) - 1);
+        while(r < m) {
+            char rightChar = s.charAt(r);
+            if(hashArr[rightChar] > 0) {
+                count++;
             }
-            right++;
-
-            // When all characters matched
-            while (count == 0) {
-                if (right - left < minLen) {
-                    minLen = right - left;
-                    start = left;
-                }
-                char temp = s.charAt(left);
-                if (map.containsKey(temp)) {
-                    map.put(temp, map.get(temp) + 1);
-                    if (map.get(temp) > 0) {
-                        count++;
-                    }
-                }
-                left++;
-            }
+            hashArr[rightChar]--;
         }
 
-        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
-    }
 
-    public static void main(String[] args) {
-        String s = "ADOBECODEBANC";
-        String t = "ABC";
-
-        String result = minWindow(s, t);
-        System.out.println("Minimum window substring: " + result);
     }
 }
