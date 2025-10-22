ccc3d8189fce4cdde982f0138ed21845456123a5
unknown <arumansalik45@gmail.com>
2025-10-22 10:51:58 +0530

    Max Product SubArray

--- KadaneAlgorithm/ProblemSet/MaxProductSubarray.java
+++ KadaneAlgorithm/ProblemSet/MaxProductSubarray.java
@@ -2,11 +2,10 @@ package KadaneAlgorithm.ProblemSet;
 
 public class MaxProductSubarray {
     public static int maxProduct(int[] nums) {
-        int maxProd = nums[0], minProd = nums[0], ans = nums[0];
+        int minProd = nums[0], maxProd = nums[0], ans = nums[0];
         for (int i = 1; i < nums.length; i++) {
             int x = nums[i];
-            // when x is negative swap max and min
-            if (x < 0) {
+            if(x < 0) {
                 int tmp = maxProd;
                 maxProd = minProd;
                 minProd = tmp;
